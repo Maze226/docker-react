780f1f83f35a96d9cd17940a3457bfd585759eaf
Michael Garagaty <michaelgaragaty@Michaels-MacBook-Air.local>
2021-05-06 21:42:29 +0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Privet! <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hi! this app is now on AWS!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
